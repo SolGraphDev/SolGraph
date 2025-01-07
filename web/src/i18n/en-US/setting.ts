a9ccd902822c4c192c3a27d1bc8f902ea356a00a
r9d9RobertVilvtp78c2flareal <Eli2asBaker@outlook.com>
2025-01-07 23:42:58 +0800

    Restructure repository layout for clarity

---
+++ web/src/i18n/en-US/setting.ts
@@ -0,0 +1,29 @@
+const translation = {
+  create: "Create",
+  team: "Team",
+  skill: "Tools",
+  user: "User",
+  knowledge: "Knowledge",
+  modal: {
+    setting: "Settings",
+    helpDocu: "Help Document",
+    logOut: "Log Out",
+  },
+
+  setting: {
+    workSpace: "Workspace",
+    modelProvider: "Model",
+    member: "Members",
+    theme: "Theme",
+    account: "Account",
+    myAccount: "My Account",
+    password: "Password",
+    language: "Language",
+    showmodel: "Show Model",
+    hidemodel: "Hide Model",
+    themedark: "Dark Mode",
+    themelight: "Light Mode",
+  },
+};
+
+export default translation;
