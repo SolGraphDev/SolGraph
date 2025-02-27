3e5a79dc402d39b15e029473fd56909d2a5743f7
r9d9RobertVilvtp78c2flareal <Eli2asBaker@outlook.com>
2025-02-27 20:14:03 +0800

    Implement feature to enhance system capabilities

---
+++ web/src/client/schemas/$UserCreate.ts
@@ -0,0 +1,33 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export const $UserCreate = {
+    properties: {
+        email: {
+            type: 'string',
+            isRequired: true,
+        },
+        is_active: {
+            type: 'boolean',
+        },
+        is_superuser: {
+            type: 'boolean',
+        },
+        full_name: {
+            type: 'any-of',
+            contains: [{
+                type: 'string',
+            }, {
+                type: 'null',
+            }],
+        },
+        language: {
+            type: 'string',
+        },
+        password: {
+            type: 'string',
+            isRequired: true,
+        },
+    },
+} as const;
